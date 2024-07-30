7d91cd309b0140ba18fcac92d6b78e6cdb47671c
photohost <127531284+photohost@users.noreply.github.com>
2024-07-30 23:18:23 +0800

    Create counter.js

---
+++ static/counter.js
@@ -0,0 +1,16 @@
+function createBSZ() {
+    var postBody = document.getElementById('postBody');
+    if (postBody){
+        postBody.insertAdjacentHTML('afterend','<div id="busuanzi_container_page_pv" style="display:none;float:left;margin-top:8px;font-size:small;">本文被偷看次數<span id="busuanzi_value_page_pv"></span>次</div>');
+    }
+    var runday = document.getElementById('runday');
+    runday.insertAdjacentHTML('afterend', '<div id="busuanzi_container_site_pv" style="display:none;">共多少人偷看<span id="busuanzi_value_site_pv"></span>次 • </div>');
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    createBSZ();
+    var element = document.createElement('script');
+    element.src = 'https://vercount.one/js';
+    document.head.appendChild(element);
+    console.log("\n %c GmeekBSZ Plugins https://github.com/Meekdai/Gmeek \n","padding:5px 0;background:#bc4c00;color:#fff");
+});
